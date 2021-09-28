c8d26ecff920ecf2d8fdf01111fef792ef8f0d91
Tarcísio Sousa <sousa.tarcisio.s@gmail.com>
2021-09-27 23:30:06 -0300

    Configuração de desacoplar DEBUG

--- core/settings.py
+++ core/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from decouple import config
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&g-0sfrl#ec&&(zb^hwcdcl&2qp2wv7z3#rv!vd%=tylf_9t^q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
