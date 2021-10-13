df05c2b6e9e070853ffe3d29efe99f705a07d9d1
Tarcísio Sousa <sousa.tarcisio.s@gmail.com>
2021-10-13 01:56:14 -0300

    Refatorando métodos para Generic Views

--- core/base/urls.py
+++ core/base/urls.py
@@ -2,16 +2,17 @@ from django.urls import path
 from . import views
 
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.urlpatterns import format_suffix_patterns
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
-
     path('login/', obtain_auth_token, name='login'),
-    path('registrar/', views.registrar, name='registrar'),
-    path('especialidades/', views.especialidades, name='especialidades'),
-    path('medicos/', views.medicos, name='medicos'),
-    path('agendas/', views.agendas, name='agendas'),
-    path('consultas/', views.consultas, name='consultas'),
-    path('consultas/<int:consulta_id>', views.consultas, name='consultas'),
+    path('registrar/', views.RegistrarList.as_view()),
+    path('especialidades/', views.EspecialidadeList.as_view()),
+    path('medicos/', views.MedicoList.as_view()),
+    path('agendas/', views.AgendaList.as_view()),
+    path('consultas/', views.ConsultaList.as_view()),
+    path('consultas/<int:pk>', views.ConsultaDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
